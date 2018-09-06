a89691ae41728fe532b0139a53097d734bc8f3e2
Nick <nbeckmann@innovive.com>
2018-09-06 16:47:54 -0700

    Add a bug and update the ignore file

--- src/HelloGit.c
+++ src/HelloGit.c
@@ -11,7 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(int argc, char **argv) {
+
+	signed char i = 0;
+
+	for (i = 0; i < 255; i++) {
+		printf("%d: !!!Hello World!!!\n", i); /* prints !!!Hello World!!! */
+	}
 	return EXIT_SUCCESS;
 }
